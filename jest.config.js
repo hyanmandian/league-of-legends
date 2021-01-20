ee67ce0489a0800cb28817f3b40e93ba1c433682
Hyan Mandian <hyan_mandian@sicredi.com.br>
2021-01-20 01:48:18 -0300

    chore: adjust some files

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   projects: [
     '<rootDir>/libs/design-system',
-    <rootDir>/apps/server,
+    '<rootDir>/apps/server',
     '<rootDir>/apps/web',
   ],
 };
-,"<rootDir>/apps/webdfd","<rootDir>/libs/xcasddas"

cd9296ddea3f77f7062b2c27cfabeea152795d16
Hyan Mandian <hyan_mandian@sicredi.com.br>
2021-01-20 11:56:43 -0300

    chore(web): add bundle analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 const withPreact = require('next-plugin-preact');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = withPreact(withNx({}));
+module.exports = withBundleAnalyzer(withPreact(withNx({})));
